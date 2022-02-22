4b93b85b873e0723b428899982abf4afdcb9e4d6
James Andariese <james@strudelline.net>
2022-02-22 09:04:26 -0600

    switch to zerolog_cli_adapter to eliminate flag boilerplate

--- cmd/vault-tls-bootstrap/main.go
+++ cmd/vault-tls-bootstrap/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	vaultlogin "github.com/jamesandariese/hashistack-node-bootstrapper/internal/cmd/vault-tls-bootstrap"
+	zerolog_cli "github.com/jamesandariese/zerolog_cli_adapter"
 	"github.com/rs/zerolog"
 )
 
@@ -14,18 +15,8 @@ func main() {
 
 	p, flagSet := vaultlogin.NewProgram()
 
-	var verbose bool
-	var debug bool
-	var trace bool
-	var quiet bool
-	var silent bool
-
-	flagSet.BoolVar(&verbose, "verbose", false, "Verbose logging")
-	flagSet.BoolVar(&verbose, "v", false, "Verbose logging")
-	flagSet.BoolVar(&debug, "debug", false, "Debug level logging")
-	flagSet.BoolVar(&trace, "trace", false, "Trace level logging")
-	flagSet.BoolVar(&quiet, "quiet", false, "Log warnings and errors only")
-	flagSet.BoolVar(&silent, "silent", false, "No logs and only show cubbyhole token at end")
+	lg := zerolog_cli.NewLoggerGenerator(logger)
+	lg.UpdateFlagSet(flagSet)
 
 	if err := flagSet.Parse(os.Args[1:]); err != nil {
 		if err == flag.ErrHelp {
@@ -35,23 +26,7 @@ func main() {
 		os.Exit(2)
 	}
 
-	if verbose {
-		logger = logger.Level(zerolog.DebugLevel)
-	}
-	if debug {
-		logger = logger.Level(zerolog.DebugLevel).With().Caller().Logger()
-	}
-	if trace {
-		logger = logger.Level(zerolog.TraceLevel).With().Caller().Logger()
-	}
-	if quiet {
-		logger = logger.Level(zerolog.WarnLevel)
-	}
-	if silent {
-		logger = zerolog.Nop()
-	}
-
-	p.SetLogger(logger)
+	p.SetLogger(lg.Logger())
 
 	os.Exit(p.RunCLI())
 }
